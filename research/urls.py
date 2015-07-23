7124f0c94250e522e9016f13f015d19a86225c76
codermango <codermango@gmail.com>
2015-07-23 10:49:10 +0200

    Rename project name

--- research/urls.py
+++ research/urls.py
@@ -1,4 +1,4 @@
-"""web_recommender URL Configuration
+"""research URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'recommender.views.index', name='index'),
-    url(r'^recommend/$', 'recommender.views.recommend'),
+    url(r'^recommender/$', 'recommender.views.index', name='index'),
+    url(r'^recommender/recommend/$', 'recommender.views.recommend'),
 ]
