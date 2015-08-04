a17554f19b5af7f720e6a0392353fde9fdbd3bc8
codermango <codermango@gmail.com>
2015-08-04 14:30:37 +0200

    We can get imdbid_feature_dict from database, so the imdbid_feature_dict files are not needed

--- recommender_libs/recommender_db.py
+++ recommender_libs/recommender_db.py
@@ -3,18 +3,37 @@ from pymongo import MongoClient
 
 
 class RecommenderDB:
-
     db = None
+    collection = None
 
     def __init__(self):
         client = MongoClient()
         self.db = client.VionelMovies
+        self.collection = self.db.allMovies
 
-    def get_collections(self, name):
-        all_movies = self.db[name]
-        return all_movies
+    def get_imdbid_feature_dict(self, feature_name):
+        result_dict = {}
+        feature_key = ""
+        if feature_name == "actor":
+            feature_key = "imdbMainactors"
+        elif feature_name == "director":
+            feature_key = "imdbDirectors"
+        elif feature_name == "genre":
+            feature_key = "genres"
+        elif feature_name == "language":
+            feature_key = "language"
+        elif feature_name == "rating":
+            feature_key = "imdbRating"
+        elif feature_name == "releaseyear":
+            feature_key = "releaseYear"
+            
+        all_movies_list = list(self.collection.find({}, {"imdbId": 1, feature_key: 1, "_id": 0}))
 
+        for movie in all_movies_list:
+            imdbid = movie["imdbId"]
+            feature = movie[feature_key]
+            result_dict[imdbid] = feature
+        return result_dict
 
 recommenderdb = RecommenderDB()
-all_movies = recommenderdb.get_collections("allMovies")
-print all_movies.count()
+print recommenderdb.get_imdbid_feature_dict("genre")
